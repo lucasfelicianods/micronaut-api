a0cac28a46d9d631fea865c01936850455f32111
Lucas <lucasfelicianods@gmail.com>
2021-07-06 20:59:19 -0300

    Adicionado resources http

--- src/main/java/br/com/entity/Customers.java
+++ src/main/java/br/com/entity/Customers.java
@@ -1,8 +1,20 @@
 package br.com.entity;
 
+import net.bytebuddy.dynamic.loading.InjectionClassLoader;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+
+@Entity
 public class Customers {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "customer_id_seq")
+    @SequenceGenerator(name = "customer_id_seq", sequenceName = "customer_id_seq", allocationSize = 1)
+    private Long id;
     private String nome;
     private String idade;
 
@@ -20,5 +32,13 @@ public class Customers {
 
     public void setIdade(String idade) {
         this.idade = idade;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
     }
 }
--- src/main/java/br/com/resource/CustomersResource.java
+++ src/main/java/br/com/resource/CustomersResource.java
@@ -2,13 +2,24 @@ package br.com.resource;
 
 import br.com.entity.Customers;
 import br.com.repository.CustomersRepository;
+import io.micronaut.http.HttpResponse;
 import io.micronaut.http.MediaType;
 import io.micronaut.http.annotation.Body;
 import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Delete;
 import io.micronaut.http.annotation.Get;
 import io.micronaut.http.annotation.Post;
 
 import javax.inject.Inject;
+import javax.persistence.EntityNotFoundException;
+import javax.swing.text.html.parser.Entity;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
 
 @Controller("/name")
 public class CustomersResource {
@@ -17,8 +28,8 @@ public class CustomersResource {
     private CustomersRepository customersRepository;
 
     @Get(consumes = MediaType.APPLICATION_JSON, produces = MediaType.APPLICATION_JSON)
-    public void getName(){
-        customersRepository.findAll();
+    public Iterable<Customers> getName(){
+        return  customersRepository.findAll();
     }
 
     @Get("/{id}")
@@ -26,8 +37,19 @@ public class CustomersResource {
         return customersRepository.findById(id).get();
     }
 
-    @Post(value = " /{name}", consumes = MediaType.APPLICATION_JSON, produces = MediaType.APPLICATION_JSON)
-    public void saveCustormes(@Body Customers customers){
-         customersRepository.save(customers);
+    @Post(consumes = MediaType.APPLICATION_JSON, produces = MediaType.APPLICATION_JSON)
+    public HttpResponse<Customers> saveCustormes(@Body Customers customers){
+        return HttpResponse.created(customersRepository.save(customers));
+    }
+
+    @Delete("/{id}")
+    public HttpResponse delete(Long id){
+        var customer = customersRepository.findById(id);
+
+         if(customer.isEmpty()){
+             return HttpResponse.notFound().body("Id não encontrado");
+         }
+        customersRepository.delete(customer.get());
+        return HttpResponse.noContent();
     }
 }
